3f8aaaca1e28d7a8c1d3246d40d74e865c0f90d1
Anthony Nichols <hi@anthonynichols.me>
2023-12-20 14:16:21 -0600

    update vite to 4.5.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import {defineConfig} from 'vite';
-
 import react from '@vitejs/plugin-react-swc';
 import Unfonts from 'unplugin-fonts/vite';
+import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
